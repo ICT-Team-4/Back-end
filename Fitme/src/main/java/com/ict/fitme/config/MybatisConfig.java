96be35d025f4a49800fd18ebb5c77450337bed72
chodonghoon2 <112090479+chodonghoon2@users.noreply.github.com>
2024-01-09 10:02:42 +0900

    spring 기본 세팅

---
+++ Fitme/src/main/java/com/ict/fitme/config/MybatisConfig.java
@@ -0,0 +1,42 @@
+package com.ict.fitme.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@MapperScan(value = {"com.ict.fitme.account.dao"},sqlSessionFactoryRef ="sqlSessionFactory" )
+public class MybatisConfig {
+	
+	private final ApplicationContext applicationContext;
+	public MybatisConfig(ApplicationContext applicationContext) {
+		this.applicationContext=applicationContext;
+	}
+	
+	@Bean
+	SqlSessionFactory sqlSessionFactory(DataSource dataSource) {
+		SqlSessionFactory factory=null;
+		try {
+			SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+			factoryBean.setDataSource(dataSource);
+			factoryBean.setTypeAliasesPackage("com.ict.fitme");
+			factoryBean.setMapperLocations(applicationContext.getResources("classpath:mybatis/mapper/**/*.xml"));			
+			factory=factoryBean.getObject();
+		}
+		catch(Exception e) {e.printStackTrace();}
+		return factory;
+	}
+	
+	@Bean
+	SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+		return new SqlSessionTemplate(sqlSessionFactory);
+	}
+	
+}
