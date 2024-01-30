aa8dc01182a0837c7ae5c8010ad88a44056a9422
chodonghoon2 <112090479+chodonghoon2@users.noreply.github.com>
2024-01-31 01:37:01 +0900

    게시판 1차 발표전 로직 작성

--- security1/src/main/java/com/security/board/service/BoardService.java
+++ security1/src/main/java/com/security/board/service/BoardService.java
@@ -1,12 +1,16 @@
 package com.security.board.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.security.board.dao.BoardMapper;
+import com.security.board.dto.AccountDto;
 import com.security.board.dto.BoardDto;
+import com.security.board.dto.BoardLikesDto;
+import com.security.board.dto.FriendDto;
 
 @Service
 public class BoardService {
@@ -33,12 +37,28 @@ public class BoardService {
 		
 	}
 	
+	//사용자 정보 조회
+	public AccountDto findByUsername(String username) {
+		return boardMapper.findByUsername(username);
+	}
+	
+	//사용자 친구 정보 조회
+	public List<FriendDto> findFriendByUsername(String username) {
+		return boardMapper.findFriendByUsername(username);
+	}
+	
 	//게시글 등록
 	@Transactional
 	public int boardSave(BoardDto dto) {
 		
 		return boardMapper.save(dto);
 		
+	}
+	
+	//좋아요 버튼 클릭
+	@Transactional
+	public int like(BoardLikesDto dto) {
+		return boardMapper.incrementLikeCount(dto);
 	}
 	
 	//게시글 수정
@@ -53,6 +73,12 @@ public class BoardService {
 	public int boardDelete(BoardDto dto) {
 		
 		return boardMapper.delete(dto);
+	}
+
+	//번호로
+	public List<BoardDto> findAllByNo(Long acconutNo) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
