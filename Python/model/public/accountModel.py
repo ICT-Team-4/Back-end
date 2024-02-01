6ae2be23498d4c8950ae003496fcbe8f5301a563
chodonghoon2 <zzehdgns@naver.com>
2024-02-02 01:02:24 +0900

    쿼리문 수정

--- Python/model/public/accountModel.py
+++ Python/model/public/accountModel.py
@@ -41,8 +41,7 @@ def workout_calendar(conn, user_id):
     with conn.cursor() as cursor:
         try:
             cursor.execute(
-                f"SELECT DISTINCT to_char(TRUNC(end_postdate),'YYYY-MM-DD') time FROM calendar c JOIN workout w ON c.calendar_no = w.calendar_no WHERE account_no = :1 ORDER BY time",
-                user_id)
+                f"SELECT DISTINCT to_char(TRUNC(end_postdate),'YYYY-MM-DD') time FROM calendar c JOIN workout w ON c.calendar_no = w.calendar_no WHERE account_no = {user_id}")
             return cursor.fetchall()
         except Exception as e:
             print('모든 데이터 조회시 오류:',e)
