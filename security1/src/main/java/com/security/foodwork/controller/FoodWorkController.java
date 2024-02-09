523314c5ede09837d75444e2ffc7a1970d2f0467
chodonghoon2 <zzehdgns@naver.com>
2024-02-09 18:30:29 +0900

    accountNo로 로그인 수정

---
+++ security1/src/main/java/com/security/foodwork/controller/FoodWorkController.java
@@ -0,0 +1,48 @@
+package com.security.foodwork.controller;
+
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.security.foodwork.dto.AccountFoodWorkDto;
+import com.security.foodwork.service.FoodWorkService;
+import com.security.util.JWTOkens;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/v1")
+public class FoodWorkController {
+	
+	private FoodWorkService foodWorkService;
+	
+	
+	public FoodWorkController(FoodWorkService foodWorkService) {
+		this.foodWorkService = foodWorkService;
+	}
+	
+	@GetMapping("/foodworks/account")
+	public ResponseEntity<AccountFoodWorkDto> accountInfo(HttpServletRequest request){
+		System.out.println("Authorization");
+		String token = request.getHeader("Authorization").split(" ")[1].trim();
+		Map<String, Object> payload = JWTOkens.getTokenPayloads(token);
+		String username = payload.get("sub").toString();
+		
+		AccountFoodWorkDto accountInfo = foodWorkService.findByUsername(username);
+		
+		System.out.println(accountInfo);
+		
+		return ResponseEntity.ok().header("Content-Type", "application/json; charset=UTF-8").body(accountInfo);
+		  
+	}
+	
+	
+	
+	
+
+}
---
+++ security1/src/main/java/com/security/model/UserMapper.java
@@ -0,0 +1,26 @@
+package com.security.model;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+
+  int insertMember(UserDto dto);
+  
+  int insertSocial(UserDto dto);
+  
+  int insertMemberWithSocial(UserDto dto);
+  
+  int insertMemberInBody(UserDto dto);
+  
+  
+  UserDto findAccountByUsername(String username);
+  
+  UserDto findAccountByAccountNo(String accountNo);
+
+  UserDto findAccountByProviderId(String providerId);
+  
+  UserDto findMemberInfoByUsername(String username);
+
+  
+}
