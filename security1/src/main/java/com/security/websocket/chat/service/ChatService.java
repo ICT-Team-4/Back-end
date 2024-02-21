4f9e2fed38e51d0a19254372ea95710157bfa53d
GilMinJong <minjong020214@gmail.com>
2024-02-21 19:31:57 +0900

    채팅방 멤버 초대

--- security1/src/main/java/com/security/websocket/chat/service/ChatService.java
+++ security1/src/main/java/com/security/websocket/chat/service/ChatService.java
@@ -7,8 +7,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.security.websocket.chat.dao.ChatMapper;
 import com.security.websocket.chat.dto.ChatCommentListDto;
+import com.security.websocket.chat.dto.ChatFriendsDto;
 import com.security.websocket.chat.dto.ChatListDto;
+import com.security.websocket.chat.dto.ChatMemberDto;
 import com.security.websocket.chat.dto.ChatRoomDto;
+import com.security.websocket.chat.dto.ChatRoomFriendsDto;
 import com.security.websocket.dto.ChatDto;
 
 @Service
@@ -24,14 +27,41 @@ public class ChatService {
 	public List<ChatCommentListDto> findChatCommentByNo(int chattingNo){
 		return chatMapper.findChatCommentByNo(chattingNo);
 	}
+	public List<ChatMemberDto> findChatRoomMemberByChatNo(int chattingNo){
+		return chatMapper.findChatRoomMemberByChatNo(chattingNo);
+	}
 	
 	//채팅 dlqfur
 	public int insert(ChatDto dto) {
 		return chatMapper.insertChat(dto);
 	}
+	//채팅 초대시 친구 확인
+	public List<ChatFriendsDto> findChatFriendsByChatNo(ChatDto dto){
+		return chatMapper.findChatFriendsByChatNo(dto);
+	}
 	
 	public ChatRoomDto findChatByNo(int chattingNo) {
 		return chatMapper.findChatByNo(chattingNo);
+	}
+	
+	@Transactional
+	public int chatRoomSave(ChatRoomFriendsDto dto) {
+		int flag = 0;
+		if(dto.getAccountNo() == null) {
+			String[] friends = dto.getFriends();
+			for(String accountNo: friends) {
+				System.out.println(">>>"+accountNo);
+				dto.setAccountNo(accountNo);
+				chatMapper.chatRoomSave(dto);
+				flag++;
+			}
+		}else {
+			String accountNo = dto.getAccountNo();
+			System.out.println(">>>"+accountNo);
+			return chatMapper.chatRoomSave(dto);
+		}
+		
+		return flag;
 	}
 	
 	@Transactional
