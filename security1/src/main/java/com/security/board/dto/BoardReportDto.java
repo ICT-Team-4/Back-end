9dbadaca97f6d18df54c56b66ac78a1f63dd44cf
chodonghoon2 <112090479+chodonghoon2@users.noreply.github.com>
2024-03-02 13:46:41 +0900

    게시판 백 로직 수정

--- security1/src/main/java/com/security/board/dto/BoardReportDto.java
+++ security1/src/main/java/com/security/board/dto/BoardReportDto.java
@@ -15,15 +15,12 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-@Alias("BoardCommentDto")
-public class BoardCommentDto {
-	private String bcno;
+@Alias("BoardReportDto")
+public class BoardReportDto {
+	
 	private String bno;
 	private String accountNo;
-	private Date postDate;
-	private String bcComment;
-	private Long replyNo;
-	private Date editDate;
-	//댓글을 작성한 사용자 이름
-	private String name;
+	private String reportComment;
+	private Date reportDate;
+
 }
