403ef1130f34af1441a10ca2f49a6e72a99ee001
ParkSangNyeong <snp95@naver.com>
2024-03-03 22:53:15 +0900

    일단 세이브 방생성, 제목 등 뿌려주기 완료 박상녕 0303/10:52
    일단 세이브 방생성, 제목 등 뿌려주기 완료 박상녕 0303/10:52

--- security1/src/main/java/com/security/game/controller/GameRoomController.java
+++ security1/src/main/java/com/security/game/controller/GameRoomController.java
@@ -4,12 +4,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
-import org.apache.catalina.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,18 +22,16 @@ import com.security.game.dto.GameRoomDto;
 import com.security.game.service.GameRoomService;
 import com.security.util.JWTOkens;
 
-import ch.qos.logback.core.model.Model;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @RestController
 @CrossOrigin
 @SessionAttributes("MapListGame")
 @RequestMapping("/api/v1")
 public class GameRoomController {
 	
-//	@Autowired
     private GameRoomService gameRoomService;
     private final SessionBean sessionBean;
     
@@ -45,18 +42,21 @@ public class GameRoomController {
     }
 
     @PostMapping("/game/createRoom/{mode}")
-    public ResponseEntity<GameRoomDto> createGameRoom(@PathVariable String mode, HttpServletRequest request) {
+	public ResponseEntity<GameRoomDto> createGameRoom(@PathVariable String mode, @RequestBody GameRoomDto gameRoomDto, HttpServletRequest request) {
+
         String token = request.getHeader("Authorization");
         Map<String, Object> payload = JWTOkens.getTokenPayloads(token);
         String accountNo = payload.get("sub").toString();
 
-        GameRoomDto dto = new GameRoomDto();
-        dto.setAccountNo(accountNo);
-        dto.setGameMode(mode);
+        log.info("게임 생성 요청 받음 - mode: {}, accountNo: {}, roomName: {}", mode, accountNo, gameRoomDto.getRoomName());
 
+        gameRoomDto.setAccountNo(accountNo);
+        gameRoomDto.setGameMode(mode);
+
+        System.out.println("accountNo 맞니?: " + accountNo + ", mode: " + mode + ", roomName: " + gameRoomDto.getRoomName());
+        
         // 게임룸 생성
-        GameRoomDto roomDetails = gameRoomService.createAndRedirectGameRoom(dto);
-
+        GameRoomDto roomDetails = gameRoomService.createAndRedirectGameRoom(gameRoomDto);
         // 세션에 게임룸 정보 저장
         Map<Integer, List<GameRoomDto>> gameRooms = sessionBean.getGameRooms();
         if (gameRooms == null) {
@@ -67,10 +67,21 @@ public class GameRoomController {
         if (!gameRooms.containsKey(roomId)) {
             gameRooms.put(roomId, new ArrayList<>());
         }
+        
         gameRooms.get(roomId).add(roomDetails);
         sessionBean.setGameRooms(gameRooms);
 
         return ResponseEntity.ok(roomDetails);
+    }
+    
+    @DeleteMapping("/game/leaveRoom/{gameroomNo}")
+    public ResponseEntity<?> leaveGameRoom(@PathVariable int gameroomNo) {
+        boolean isDeleted = gameRoomService.leaveAndDeleteGameRoom(gameroomNo);
+        if(isDeleted) {
+            return ResponseEntity.ok().body("방이 삭제되었습니다.");
+        } else {
+            return ResponseEntity.ok().body("방에서 나왔습니다.");
+        }
     }
 
 
