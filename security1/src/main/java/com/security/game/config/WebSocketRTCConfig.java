8749146275ca852fd19e7b9842f287012d6398fd
ParkSangNyeong <snp95@naver.com>
2024-03-04 23:22:01 +0900

    박상녕 0304 webrtc 하던중 올림
    박상녕 0304 webrtc 하던중 올림

--- security1/src/main/java/com/security/game/config/WebSocketRTCConfig.java
+++ security1/src/main/java/com/security/game/config/WebSocketRTCConfig.java
@@ -1,27 +1,28 @@
-package com.security.websocket.config;
+package com.security.game.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketRTCConfig implements WebSocketMessageBrokerConfigurer{
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
+        registry.addEndpoint("/rtc")
                 .setAllowedOrigins("*")
                 .withSockJS();
     }
     
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/sub");//받는것
-        registry.setApplicationDestinationPrefixes("/pub");//보내는것.
+        registry.enableSimpleBroker("/sub");
+        registry.setApplicationDestinationPrefixes("/pub");
     }
-    
 }
+
+
+
--- security1/src/main/java/com/security/websocket/videogame/controller/WebRTCController.java
+++ security1/src/main/java/com/security/websocket/videogame/controller/WebRTCController.java
@@ -1,15 +1,64 @@
 package com.security.websocket.videogame.controller;
 
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 
+@Log4j2
 @Controller
 public class WebRTCController {
-	@MessageMapping("/call")
-	@SendTo("/sub/messages")
-	public String sendMessage(String message) {
-		return message;
+	
+	public WebRTCController() {
+		System.out.println("WebRTCController 호출");
 	}
+	
+	// 방 안에서의 Peer 간 offer 정보 교환
+    @MessageMapping("/room/{roomNo}/offer")
+    @SendTo("/sub/room/{roomNo}/offer")
+    public String handleOffer(@Payload String offer, @DestinationVariable("roomNo") String roomNo) {
+        log.info("[Offer] Room No????: {}, Offer????: {}", roomNo, offer);
+        System.out.println("번호 1");
+        return offer;
+    }
+
+    // 방 안에서의 Peer 간 ICE Candidate 정보 교환
+    @MessageMapping("/room/{roomNo}/ice-candidate")
+    @SendTo("/sub/room/{roomNo}/ice-candidate")
+    public String handleIceCandidate(@Payload String candidate, @DestinationVariable("roomNo") String roomNo) {
+        log.info("[ICE Candidate] Room No::::: {}, Candidate::::: {}", roomNo, candidate);
+        System.out.println("번호 2");
+        return candidate;
+    }
+
+    // 방 안에서의 Peer 간 Answer 정보 교환
+    @MessageMapping("/room/{roomNo}/answer")
+    @SendTo("/sub/room/{roomNo}/answer")
+    public String handleAnswer(@Payload String answer, @DestinationVariable("roomNo") String roomNo) {
+        log.info("[Answer] Room No++++: {}, Answer++++: {}", roomNo, answer);
+        System.out.println("번호 3");
+        return answer;
+    }
+
+    // 방에 참여하기 위한 키 발송
+    @MessageMapping("/call/key")
+    @SendTo("/sub/call/key")
+    public String callKey(@Payload String message) {
+        log.info("[Key]==: {}", message);
+        System.out.println("번호 4");
+        return message;
+    }
+
+    // 모든 연결된 세션에 자신의 키를 보내기
+    @MessageMapping("/send/key")
+    @SendTo("/sub/send/key")
+    public String sendKey(@Payload String message) {
+        log.info("[Send Key]!!!!: {}", message);
+        System.out.println("번호 5");
+        return message;
+    }
 }
